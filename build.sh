b6a8faa45a615fed31afb9b4ca17fddac4129744
RoMeAh <runa.ronit3129@gmail.com>
2021-05-11 18:32:15 +0530

    ✅ build(xargo): introduced xargo and fixed shell script

--- build.sh
+++ build.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
-cargo build --release
-
 if [ "$OS" = "Windows_NT" ]; then
-    target = "./target/release/blazescript.exe"
+	target="x86_64-pc-windows-msvc"
+    exe="blazescript.exe"
+    executable="target/$target/release/$exe"
 else
-    target = "./target/release/blazescript"
+	case $(uname -sm) in
+	"Darwin x86_64") target="x86_64-apple-darwin" ;;
+	"Darwin arm64") target="aarch64-apple-darwin" ;;
+	*) target="x86_64-unknown-linux-gnu" ;;
+	esac
+    exe="blazescript"
+    executable="target/$target/release/$exe"
 fi
-strip $target
+
+rustup toolchain install nightly
+rustup override set nightly
+rustup component add rust-src
+cargo install xargo
+xargo build --release --target $target
+strip $executable
+
+if [ ! -d "bin" ]; then
+	mkdir -p "bin"
+fi
+if [ -f "bin/blazescript" ]; then
+    rm -r "bin/blazescript"
+fi
+
+mv $executable bin/
